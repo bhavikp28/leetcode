401376a3b46b6a1f514ac9466ea1e15057533ab3
Bhavik Patel <patelbhavik063@gmail.com>
2022-10-02 00:58:36 -0400

    Time: 1764 ms (8.97%), Space: 13.9 MB (9.37%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def fibo(n):
+            if n <= 1:
+                return n
+            return fibo(n-1) + fibo(n-2)
+        return fibo(n)

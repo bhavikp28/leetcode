f093f1f0142ec30c9c902c9c21be7c41fa4fad57
Bhavik Patel <patelbhavik063@gmail.com>
2022-10-24 15:45:31 -0400

    Time: 359 ms (99.56%), Space: 15.5 MB (24.52%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,25 +1,21 @@
 import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        left = 1
-        right = max(piles)
-        k = max(piles)
+          # Binary Search
+        # l, r = 1, max(piles)
+        # HIS INNOVATION! 
+        n, maxp, sump = len(piles), max(piles), sum(piles)
+        if n == h: return maxp
+        l, r = min(maxp, (sump-1)//h+1), min(maxp, (sump-1)//(h-n)+1)
+        # l, r = (sump-1)//h+1, maxp
+        if len(piles) == h: return r
 
-        def guess(n):
-            hour = 0
-            for i in piles:
-                hour += math.ceil(i / n)
-            #print(hour,n)
-            if hour > h:
-                return 1
-            elif hour <= h:
-                return -1
-
-        while left <= right:
-            mid = (left + right) // 2
-            if guess(mid) == 1:
-                left = mid + 1
-            elif guess(mid) == -1:
-                k = min(k,mid)
-                right = mid - 1
-        return k
+        def time_taken(k):
+            return sum(math.ceil(p/k) for p in piles)
+        while l < r: 
+            # print(f"l,r: {l,r}")
+            mid = (l+r)//2
+            time_mid = time_taken(mid)
+            if time_mid > h: l = mid + 1
+            elif time_mid <= h: r = mid            
+        return r

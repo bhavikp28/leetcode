74ec3ac5abe8aee79e8129594c3431954418c880
Bhavik Patel <patelbhavik063@gmail.com>
2023-05-07 03:02:57 -0400

    Time: 308 ms (15.12%), Space: 16.9 MB (9.14%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,20 +1,19 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         hmap = collections.defaultdict(int)
+        hmap = collections.defaultdict(int)
         for i in chars:
             hmap[i] += 1
-        ans = []
+        count = 0
         for word in words:
+            goodword = True
             hmap2 = collections.defaultdict(int)
-            count = 0
             for letter in word:
                 hmap2[letter] +=1
                 if hmap[letter] >= hmap2[letter]:
-                    count += 1
-            if count == len(word):
-                ans.append(word)
-        total = 0
-        for i in ans:
-            for a in i:
-                total += 1
-        return total
+                    pass
+                else:
+                    goodword = False
+            if goodword == True:
+                count += len(word)
+        return(count)        
